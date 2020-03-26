887f579cce554bccbc0bc5d82412eaf9c250a780
JUAN M RESTREPO <tinto922@gmail.com>
2020-03-26 13:43:58 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,22 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0 
+  new_array = []
+  
+  while collection.length > i 
+  new_array << yield(collection[i])
+  
+  i = i + 1
+  end 
+  if new_array.include?(false)
+    false
+  else 
+    true 
+  end 
 end
+
+
+my_all?([1, 2, 3]) { |num| 
+  num < 4
+}
